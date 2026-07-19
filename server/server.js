f699190359058993fea310aa6696b51a6f3d19fd
latika-15 <lavitewari15@gamil.com>
2026-07-19 23:32:48 +0530

    Setup Express server architecture and MongoDB configuration

--- server/server.js
+++ server/server.js
@@ -1,21 +1,17 @@
-import express from "express";
-import cors from "cors";
 import dotenv from "dotenv";
+
+import app from "./app.js";
+
+import connectDB from "./config/db.js";
 
 dotenv.config();
 
-const app = express();
-
-app.use(cors());
-
-app.use(express.json());
-
-app.get("/", (req, res) => {
-    res.send("Portfolio API Running");
-});
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+app.listen(PORT,()=>{
+
+console.log(`Server running on ${PORT}`);
+
 });
